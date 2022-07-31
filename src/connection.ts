248b56fc13e969506cf8288eb72a817e184d5180
Daniel Pordeus Gonzalez <danielpordeusgonzalez@hotmail.com>
2022-07-31 16:30:27 -0300

    feat: cria uma connection

---
+++ src/connection.ts
@@ -0,0 +1,20 @@
+import mysql from 'mysql2/promise';
+
+const {
+  DB_HOST,
+  DB_USER,
+  DB_PASS,
+  DB_NAME,
+  DB_PORT
+
+} = process.env;
+
+export const db = mysql.createPool({
+  host: DB_HOST,
+  port:Number(DB_PORT),
+  user: DB_USER,
+  password: DB_PASS,
+  database: DB_NAME,
+
+});
+
