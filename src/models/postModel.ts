c5fd37c329f9a82eeb01a540996e09eda454f135
Daniel Pordeus Gonzalez <danielpordeusgonzalez@hotmail.com>
2022-08-04 04:16:10 -0300

    feat: cria model Post

--- src/models/postModel.ts
+++ src/models/postModel.ts
@@ -1,45 +1,50 @@
 import { ResultSetHeader } from 'mysql2/promise';
-import { db } from '../connection';
+import database  from '../connection';
 import { Post } from '../interfaces/postInterface';
 
-const TABLE = 'post';
 
 //selecionar todas as postagens
 export const postModel = {
   async getAll(): Promise<Post[]> {
-    const sql = `
+    const query = `
     select
     id,
     message,
-    userId
-    from ${TABLE}
+    user_id as userId
+    from db.posts
     `;
-    const [rows] = await db.query(sql);
+    const [rows] = await database.query(query);
     return rows as Post[];
-  }
-};
+  },
+  //cria uma postagem
 
-//cria uma postagem
-
-export const createPost = {
   async create(post: Post): Promise<Post> {
     const { message,  userId} = post;
-    const result = await db.execute<ResultSetHeader>(
-      'INSERT INTO db.Users (message, userId) VALUES (?, ?)',
+    const result = await database.execute<ResultSetHeader>(
+      'INSERT INTO db.posts (message, user_id ) VALUES (?, ?)',
       [message, userId],
     );
     const [dataInserted] = result;
     const {insertId} = dataInserted;
     return { id: insertId, ...post };
-  }
+  },
+  //editar uma postagem
+
+  async update(id: number, message: string){
+    const query ='UPDATE db.posts SET `message` = ? WHERE `id` = ?';
+    const post = await database.execute(query, [message, id]);
+    console.log(message, id);
+    console.log(post);
+    return (post);
+
+  },
+  //deletar uma postagem
+  async remove(id: number) {
+    const query = ' DELETE FROM db.posts WHERE `id` = ?';
+    await database.execute(query, [id]);
+    return true;
+  },
+
 };
 
-//editar uma postagem
-export const updatePost = {
-  async update(){
-    console.log('pra n dar errror nvoamente');
-  }
-};
-
-
-//deletar uma postagem
+export default postModel;
