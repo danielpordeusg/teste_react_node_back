d1f3b0aa785f4f6871e671b5b7deb968500092c5
Daniel Pordeus Gonzalez <danielpordeusgonzalez@hotmail.com>
2022-08-04 04:16:31 -0300

    feat:  User model feito

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,14 +1,24 @@
 import { ResultSetHeader } from 'mysql2/promise';
-import { db } from '../connection';
+import database  from '../connection';
 import { User } from '../interfaces/userInterface';
 
-const TABLE = 'user';
 
 const userModel = {
+  async getAll(): Promise<any> {
+    try {
+      const result = await database
+        .execute('SELECT * FROM db.users');
+      const [rows] = result;
+      console.log(rows);
+      return rows as User[];
+    } catch (error) {
+      return error;
+    }
+  },
   async create(user: User): Promise<User> {
     const { name, email, password } = user;
-    const result = await db.execute<ResultSetHeader>(
-      'INSERT INTO db.Users (name, email, password) VALUES (?, ?, ?)',
+    const result = await database.execute<ResultSetHeader>(
+      'INSERT INTO db.users (name, email, password) VALUES (?, ?, ?)',
       [name, email, password],
     );
     const [dataInserted] = result;
@@ -16,17 +26,10 @@ const userModel = {
     return { id: insertId, ...user};
   },
   async login(email: string, password:string) {
-    const query = `SELECT 
-    id 
-    FROM 
-    db.users
-    WHERE
-    email = ?, password = ?
-    `;
-    const result = await db.execute(query, [email, password]);
-    console.log(result);
-    return result;
-    
+    const query = 'SELECT id, email, password FROM db.users WHERE `email`=? and `password`=?' ;
+    const [result] = await database.execute(query, [email, password]);
+    const user = (result as User[])[0];
+    return user;
   }
 };
 
