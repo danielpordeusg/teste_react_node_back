5dcaa8c9836dd7eadf47a7c1bf269ff12bdf9855
Daniel Pordeus Gonzalez <danielpordeusgonzalez@hotmail.com>
2022-08-04 04:14:29 -0300

    feat: cria controller da rota User

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,19 +1,32 @@
-import {Request, Response} from 'express';
+import {Request, Response,NextFunction} from 'express';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 import userService from '../services/userService';
 
-const userController = {
+dotenv.config();
 
-  async create(req:Request, res:Response){
-    const userinfo = req.body;
-    const {password, ...user }= await userService.create(userinfo);
-    return res.status(201).json(user);
-  },
-  async login(req:Request, res: Response){
-    const { email, password } = req.body;
-    if (email || password) {
-      console.log('pra n dar error');
+const secret: string = process.env.SECRET || 'chave';
+
+const userController = {
+  async getAll (_req: Request, res: Response, next: NextFunction){
+    try {
+      const user = await userService.getAll();
+      return res.status(200).json(user);
+    } catch (error) {
+      return next(error);
     }
-  }
+  },
+  async create(req:Request, res:Response, next: NextFunction){
+    try {
+      const user = req.body;
+      console.log(user);
+      const saveUser = await userService.create(user);
+      const token = jwt.sign({data: user}, secret);
+      return res.status(201).json(token);
+    } catch (error) {
+      return next(error);
+    }
+  },
 };
 
 export default userController;
