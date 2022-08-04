62245d2acd36c3dc415e40d9cdc78ce77cab91d0
Daniel Pordeus Gonzalez <danielpordeusgonzalez@hotmail.com>
2022-08-04 04:13:00 -0300

    feat: Rotas no app e adiciona cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,22 @@
 import express from 'express';
+import { errorHandlerMiddleware } from './middlewares/middleError';
 import { userRoutes } from './routes/userRouter';
+import { loginRoutes } from './routes/loginRouter';
+import postRouter  from './routes/postRouter';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 
-app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json(/*{limit:'50mb'}*/));
 
-app.use('/user', userRoutes);
+app.use('/users', userRoutes);
+app.use('/login', loginRoutes);
+app.use('/posts', postRouter);
 
 app.get('/', (_req, res) => res.sendStatus(200));
+
+app.use(errorHandlerMiddleware);
 
 export default app;
